600f13e8f734b9920e6fa0e0b5b053fd86cf429b
cadodominguez <itzkarurosu@gmail.com>
2024-02-11 11:35:59 -0600

    inicio

---
+++ Tarea1_ForySwitch_CarlosDominguez/src/tarea1_foryswitch_carlosdominguez/Tarea1_ForySwitch_CarlosDominguez.java
@@ -0,0 +1,12 @@
+
+package tarea1_foryswitch_carlosdominguez;
+
+public class Tarea1_ForySwitch_CarlosDominguez {
+ 
+    public static void main(String[] args) {
+    
+        
+      
+    }
+    
+}
